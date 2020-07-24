8f86f0770035bc572c80013c67b3a864d75d7861
nanettetaylor <nanette.m.taylor@gmail.com>
2020-07-24 10:19:23 +0000

    Some minor updates

--- client/src/components/GetService.js
+++ client/src/components/GetService.js
@@ -7,17 +7,17 @@ import Login from './Login'
 class GetService extends React.Component {
 
   constructor(props) {
-    super(props)
-    this.state = {
-        showBooking: false,
-        show: false,
-        searchLocality: "",
-        clickedService: {},
-        userId: 0,
-        time: "",
-        date: "",
-        shouldLogin: false
-    }
+      super(props)
+      this.state = {
+          showBooking: false,
+          show: false,
+          searchLocality: "",
+          clickedService: {},
+          userId: 0,
+          time: "",
+          date: "",
+          shouldLogin: false
+      };
   }
   componentDidMount() {
       if(this.props.providersList.length > 0){
@@ -26,10 +26,10 @@ class GetService extends React.Component {
 
   }
 
-    handleModalShowing = () => {
-    this.setState({
-      showBooking: !this.state.showBooking
-    })
+  handleModalShowing = () => {
+      this.setState({
+          showBooking: !this.state.showBooking
+      });
   }
   handleClick = () => {
     this.setState({
@@ -37,17 +37,16 @@ class GetService extends React.Component {
     })
   }
   setShow = () => {
-    this.setState({
-      show: !this.state.show,
-      showBooking: false
-    })
-    
+      this.setState({
+          show: !this.state.show,
+          showBooking: false
+      });
   }
   handleChange = (e) => {
-    const { name, value } = e.target;
-    this.setState({
-      [name] : value
-    })
+      const { name, value } = e.target;
+      this.setState({
+          [name]: value
+      });
   }
   handleServiceSelect(provider){
       let cookieArr = [];
@@ -85,7 +84,7 @@ class GetService extends React.Component {
               cookieData[data[0].trim()] = decodeURIComponent(data[1]);
           })
       }
-      // TODO: Route booking to backend
+      // Route booking to backend
       let response = await fetch("/services/book", {
           method: "POST",
           headers: {
@@ -114,80 +113,89 @@ class GetService extends React.Component {
                </div>
            )
        }
-    const usDollar = {
-    style: "currency",
-    currency: "USD"
-    }
-   
-    return (
-      <div className="position-relative overflow-hidden">
-          <div className="text-center mb-5"><h1><span>Do-ers in {this.state.searchLocality}</span></h1></div>
-          <Alert show={this.state.show} variant="success">
-              <div className="alert-close">
-                  <p>Your booking request has been sent to the do-er.</p>
-                  <p>{this.state.time} on {this.state.date} is booked.</p>
-                  <Button variant="outline-success" onClick={() => this.setShow()}>close</Button>
-              </div>
-          </Alert>
-          <Alert show={this.state.shouldLogin} variant="danger">
-          <div className="alert-close" style={{ alignItems: 'center' }}>
-              <span>Please sign up or login to book.</span>
-              <Button variant="outline-danger" onClick={() => this.setState({ shouldLogin: false })}>close</Button>
-          </div>
-      </Alert>
-          <div className="row">
-          <div className="row border border-secondary">
+       const usDollar = {
+           style: "currency",
+           currency: "USD"
+       };
 
-              <div className="col-md-5">
-                  {this.props.providersList.map(data => {
-                      const currency = data.price.toLocaleString('en-US', usDollar)
-                      return (
-                          <div className="card">
-                              <div className="card-body mb-4">
-                                  <div className="row">
-                                      <div className="media">
-                                          <img src={data.profile_img} className="mr-3" alt={data.service_owner} />
-                                              <div className="media-body">
-                                                  <h5 className="mt-0">{data.service_owner} <img src={`./icons/${data.service_type}.png`} alt={`${data.service_type} icon`}/></h5>
-                                                  <p>{data.description}</p>
-                                                  <h6>{currency}</h6>
-                                                  <button className="btn btn-sm btn-primary" onClick={()=>this.handleServiceSelect(data)}>Book Service</button>
-                                              </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      )
-                  })}
-              </div>
-              <div className="col-md-7">
-                  <MapView services = {this.props.providersList}/>
-              </div>
-          </div>
-        <Modal show={this.state.showBooking}
-           onHide={() => this.handleModalShowing()}>
-            <Modal.Header>
-                <Modal.Title>Book {this.state.clickedService.service_owner}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <div className="container">
-                    <div className="form-group">
-                        <label for="date">Pick a date</label>
-                        <input type="date" onChange={e => {this.handleChange(e)}} className="form-control" id="date" name="date" />
-                    </div>
-                    <div className="form-group">
-                        <label for="time">Select a time</label>
-                        <input type="time" onChange={e => {this.handleChange(e)}} className="form-control" id="time" name="time" />
-                    </div>
-                    <div className="form-group">
-                        <Button onClick={() => this.handleBookSubmit(this.state.clickedService)} className="btn btn-success rounded ">Book Now</Button>
-                    </div>
-                </div>
-            </Modal.Body>
-        </Modal>
-      </div>
-      </div>
-    );
+       return (
+           <div className="position-relative overflow-hidden">
+               <div className="text-center mb-5"><h1><span>Do-ers in {this.state.searchLocality}</span></h1></div>
+               <Alert show={this.state.show} variant="success">
+                   <div className="alert-close" style={{alignItems: 'center'}}>
+                       <p>Your booking request has been sent to the do-er.</p>
+                       <p>{this.state.time} on {this.state.date} is booked.</p>
+                       <Button variant="outline-success" onClick={() => this.setShow()}>close</Button>
+                   </div>
+               </Alert>
+               <Alert show={this.state.shouldLogin} variant="danger">
+                   <div className="alert-close" style={{alignItems: 'center'}}>
+                       <span>Please sign up or login to book.</span>
+                       <Button variant="outline-danger"
+                               onClick={() => this.setState({shouldLogin: false})}>close</Button>
+                   </div>
+               </Alert>
+               <div className="row border border-secondary">
+
+                   <div className="col-md-5">
+                       {this.props.providersList.map(data => {
+                           const currency = data.price.toLocaleString('en-US', usDollar)
+                           return (
+                               <div className="card">
+                                   <div className="card-body mb-4">
+                                       <div className="row">
+                                           <div className="media">
+                                               <img src={data.profile_img} className="mr-3" alt={data.service_owner}/>
+                                               <div className="media-body">
+                                                   <h5 className="mt-0">{data.service_owner} <img
+                                                       src={`./icons/${data.service_type}.png`}
+                                                       alt={`${data.service_type} icon`}/></h5>
+                                                   <p>{data.description}</p>
+                                                   <h6>{currency}</h6>
+                                                   <button className="btn btn-sm btn-primary"
+                                                           onClick={() => this.handleServiceSelect(data)}>Book Service
+                                                   </button>
+                                               </div>
+                                           </div>
+                                       </div>
+                                   </div>
+                               </div>
+                           )
+                       })}
+                   </div>
+                   <div className="col-md-7">
+                       <MapView services={this.props.providersList}/>
+                   </div>
+               </div>
+               <Modal show={this.state.showBooking}
+                      onHide={() => this.handleModalShowing()}>
+                   <Modal.Header>
+                       <Modal.Title>Book {this.state.clickedService.service_owner}</Modal.Title>
+                   </Modal.Header>
+                   <Modal.Body>
+                       <div className="container">
+                           <div className="form-group">
+                               <label for="date">Pick a date</label>
+                               <input type="date" onChange={e => {
+                                   this.handleChange(e)
+                               }} className="form-control" id="date" name="date"/>
+                           </div>
+                           <div className="form-group">
+                               <label for="time">Select a time</label>
+                               <input type="time" onChange={e => {
+                                   this.handleChange(e)
+                               }} className="form-control" id="time" name="time"/>
+                           </div>
+                           <div className="form-group">
+                               <Button onClick={() => this.handleBookSubmit(this.state.clickedService)}
+                                       className="btn btn-success rounded ">Book Now</Button>
+                           </div>
+                       </div>
+                   </Modal.Body>
+
+               </Modal>
+           </div>
+       );
   }
 }
 
