dd383feae6f768a272e83b5c431bd7db4b97573b
JM066 <mina.lee066@gmail.com>
2020-11-25 21:07:10 +0100

    send all the routes to build

--- app.js
+++ app.js
@@ -24,19 +24,6 @@ app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 app.set('view engine', 'ejs');
 
-app.set('port', (process.env.PORT || 5000));
-//send all the routes to client/build/index.html file
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
-//For avoidong Heroku $PORT error
-// app.get('/', function(request, response) {
-//     var result = 'App is running'
-//     response.send(result);
-// }).listen(app.get('port'), function() {
-//     console.log('App is running, server is listening on port ', app.get('port'));
-// });
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -46,8 +33,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false
 }));
-//all the static assets will be located in client build 
-app.use(express.static(path.join(__dirname, '/client/build')));
+
 
 //Initialise passport
 app.use(passport.initialize());
@@ -68,12 +54,16 @@ app.use(
   })
 )
 
+//all the static assets will be located in client build 
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.static('public'));
-// app.get('/', (req,res) => {
-//     // res.render('home');
-//     res.send("Welcome to the backend");
-//     // res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+
+//send all the routes to client/build/index.html file
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + 'build', 'index.html'));
+});
+
 
 
 // catch 404 and forward to error handler
