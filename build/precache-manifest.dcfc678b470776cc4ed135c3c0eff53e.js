778b11fe9c99182ce46cd2e4a058fbaad4236072
JM066 <mina.lee066@gmail.com>
2020-11-28 14:28:43 +0100

    front end built on back end

---
+++ build/precache-manifest.dcfc678b470776cc4ed135c3c0eff53e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "085f7e3f0c7021deed60fd08153e62d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "100680d0c33a9ef9ff82",
+    "url": "/static/css/2.bab700ec.chunk.css"
+  },
+  {
+    "revision": "e3b54d0eeee6cac3e5f5",
+    "url": "/static/css/main.b7844165.chunk.css"
+  },
+  {
+    "revision": "100680d0c33a9ef9ff82",
+    "url": "/static/js/2.1310e3f0.chunk.js"
+  },
+  {
+    "revision": "cdef75879235130052acc2626c717c44",
+    "url": "/static/js/2.1310e3f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3b54d0eeee6cac3e5f5",
+    "url": "/static/js/main.384237b3.chunk.js"
+  },
+  {
+    "revision": "fb872b8d1116b5695b55ee05c851a053",
+    "url": "/static/js/main.384237b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "3d06a355ff334faa5ab311fcdc1fe6eb",
+    "url": "/static/media/bg_03.3d06a355.svg"
+  }
+]);
