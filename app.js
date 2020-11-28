12c790b3e1c8f1e90efb478ea760b7971a41ffb3
JM066 <mina.lee066@gmail.com>
2020-11-28 14:54:34 +0100

    fixing router not working on heroku

--- app.js
+++ app.js
@@ -59,8 +59,14 @@ app.use(
 )
 
 //send all the routes to client/build/index.html file
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + 'build', 'index.html'));
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname + 'build', 'index.html'));
+// });
+app.get('/*', (req, res) => {
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/')) // we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
 });
 
 // catch 404 and forward to error handler
