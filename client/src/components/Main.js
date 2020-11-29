d436c839575e40dcb9491dd83d338147a0c39d04
JM066 <mina.lee066@gmail.com>
2020-11-29 17:31:46 +0100

    heroku deploy error

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -96,14 +96,15 @@ export default class Main extends React.Component {
                                     <Route exact path='/' render={({ history}) => (
                                       <Link to={"/getService"}>
                                         <button className="btn btn-outline-success mb-2 btn-search" onClick={this.handleSearch}>
-                                          <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-search"
+                                          {/* <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-search"
                                                 fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                                 <path fillRule="evenodd"
                                                       d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
                                                 <path fillRule="evenodd"
                                                       d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-                                            </svg>
-                                            &nbsp;&nbsp;Search
+                                            </svg> */}
+                                            {/* &nbsp;&nbsp;Search */}
+                                            Search
                                         </button>
                                       </Link> 
                                      )} />
